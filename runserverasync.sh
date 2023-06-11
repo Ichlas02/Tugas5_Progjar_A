5a0767779a31cd95d6e50c92c9ee2887aadb5f36
Ichlasul Hasanat <88977654+Ichlas02@users.noreply.github.com>
2023-06-11 15:14:10 +0700

    Add files via upload

---
+++ runserverasync.sh
@@ -0,0 +1,6 @@
+#jalankan 4 async_server
+
+python3 async_server.py 4002 &
+python3 async_server.py 4003 &
+python3 async_server.py 4004 &
+python3 async_server.py 4001 &
---
+++ runserverpool.sh
@@ -0,0 +1,6 @@
+#jalankan 4 server_process_pool_http.py
+
+python3 server_process_pool_http.py 4005 &
+python3 server_process_pool_http.py 4006 &
+python3 server_process_pool_http.py 4007 &
+python3 server_process_pool_http.py 4008 &
